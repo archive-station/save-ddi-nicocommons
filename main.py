78bdf2d0a7ab52ff05452afbbbe0a21c9194fad3
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-05-28 09:27:49 -0700

    synzr branch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+def search(url: str):
+    git = requests.get('https://public-api.commons.nicovideo.jp/v1/materials/search/keywords?q=%E3%81%A9%E3%81%93%E3%81%A7%E3%82%82%E3%81%84%E3%81%A3%E3%81%97%E3%82%87&_limit=50&_offset=0&_sort=%2BstartTime').json()
+    print(git)
 
 def grab_metadata(url: str):
    url = url.split('nc')[1]
@@ -7,9 +10,12 @@ def grab_metadata(url: str):
    # return
    return git
 
+
+
 def download(url: str):
     metadata = grab_metadata(url)
     
 
 if __name__ == '__main__':
+    search('help')
     download('nc15531')
