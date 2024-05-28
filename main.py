9b8767deb78891a99900915b1edb792411f0c5a7
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-05-28 09:23:27 -0700

    Typo

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
-def grab_info(url: str):
+
+
+def grab_metadata(url: str):
    url = url.split('nc')[1]
    git = requests.get(f'https://public-api.commons.nicovideo.jp/v1/materials/{url}').json()
-
+   # return
    return git
 
-
 def download(url: str):
-    print(grab_info(url))
-
+    metadata = grab_metadata(url)
+    
 
 if __name__ == '__main__':
     download('nc15531')
