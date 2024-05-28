c9ee399c79f94c7e88cc60744c46f818b57c5b90
mikhail <mikhail@tskau.team>
2024-05-28 21:47:16 +0500

    wip: nicovideo class with unimplemented methods

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import requests
 
+USER_AGENT_HEADER = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+ACCEPT_HEADER = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+
+
+class Nicovideo:
+    def __init__(self) -> None:
+        self.__session = requests.Session()
+        self.__session.headers["user-agent"] = USER_AGENT_HEADER
+        self.__session.headers["accept"] = ACCEPT_HEADER
+    
+    def perform_login(self) -> None:
+        pass # TODO @synzr
+
+    def fetch_nc_material(self, id: int) -> None:
+        pass # TODO @synzr
+
+
 def search(url: str):
     git = requests.get('https://public-api.commons.nicovideo.jp/v1/materials/search/keywords?q=%E3%81%A9%E3%81%93%E3%81%A7%E3%82%82%E3%81%84%E3%81%A3%E3%81%97%E3%82%87&_limit=50&_offset=0&_sort=%2BstartTime').json()
     print(git)
