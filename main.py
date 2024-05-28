1352d43c6c226ff95e4983155790ca08d33b0809
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-05-28 09:30:33 -0700

    work on search

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 
+def search(term: str):
+    offset = 0
+    limit = 50
+    git = requests.get(f'https://public-api.commons.nicovideo.jp/v1/materials/search/keywords?q={term}&_limit=50&_offset=0&_sort=%2startTime').json()
+    print(git)
 
 def grab_metadata(url: str):
    url = url.split('nc')[1]
@@ -7,9 +12,12 @@ def grab_metadata(url: str):
    # return
    return git
 
+
+
 def download(url: str):
     metadata = grab_metadata(url)
     
 
 if __name__ == '__main__':
+    search('help')
     download('nc15531')
