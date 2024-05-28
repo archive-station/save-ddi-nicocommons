d1845698f8d9a788e8b95957b5089818dc0e3f97
ellie <81702700+yeeterlol@users.noreply.github.com>
2024-05-28 09:40:51 -0700

    working search piping to downloader

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import requests
+import pyperclip
 
 def search(term: str):
     offset = 0
     limit = 50
-    git = requests.get(f'https://public-api.commons.nicovideo.jp/v1/materials/search/keywords?q={term}&_limit=50&_offset=0&_sort=%2startTime').json()
-    print(git)
+    # count how many items
+    itemsCount = None
+
+    git = requests.get(f'https://public-api.commons.nicovideo.jp/v1/materials/search/keywords?q={term}&_limit=50&_offset=0&_sort=%2BstartTime').json()
+    data = git.get("data")
+    itemsCount = data.get("total")
+    items = data.get("materials")
+    # print(items)
+    for item in items:
+        global_id = item.get('global_id')
+        title = item.get("title")
+        print(f'[-] downloading {title}')
+        download(global_id)
+    
+    return ":3"
+
 
 def grab_metadata(url: str):
    url = url.split('nc')[1]
@@ -19,5 +34,5 @@ def download(url: str):
     
 
 if __name__ == '__main__':
-    search('help')
+    search('%E3%81%A9%E3%81%93%E3%81%A7%E3%82%82%E3%81%84%E3%81%A3%E3%81%97%E3%82%87')
     download('nc15531')
